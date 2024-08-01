3ee553b124f202cba97a3e24c1772d5cfeafa006
michel <michel-gouveia1997@hotmail.com>
2024-08-01 14:12:28 -0300

    update

--- js/main.js
+++ js/main.js
@@ -43,7 +43,6 @@ theToggle.onclick = function() {
 
 
 
-
 $('.nav-icon').click(function(){
     $(this).toggleClass('open')
 });
@@ -52,4 +51,29 @@ $('.nav-icon').click(function(){
     var nav = $('.js-main-nav')
 
     nav.slideToggle(200)
-})
+});
+
+
+
+
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var images = document.querySelectorAll('.clickable-image');
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+
+images.forEach(img => {
+  img.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+  }
+});
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() { 
+  modal.style.display = "none";
+}
